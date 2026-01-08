8aa9e0a663ed0798ed13e19132ccd561035f52ae
KimJaeWoo <rlawodnv2@naver.com>
2026-01-08 23:58:29 +0900

    테스트 진행중

--- src/test/java/com/itembay/item/controller/ItemControllerTest.java
+++ src/test/java/com/itembay/item/controller/ItemControllerTest.java
@@ -7,8 +7,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
